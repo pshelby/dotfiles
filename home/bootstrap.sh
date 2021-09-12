89a744753e0d48f4a6c32a6f47ef2a7b677cb9ab
Patrick Shelby <pshelby08@gmail.com>
2021-09-12 08:40:53 -0400

    Adding functionality to install custom ZSH plugins.

--- home/bootstrap.sh
+++ home/bootstrap.sh
@@ -18,10 +18,13 @@ function doIt() {
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt;
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/N) " -n 1;
 	echo "";
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		doIt;
 	fi;
 fi;
 unset doIt;
+
+# Install ZSH plugins
+shared/zsh-plugins.sh
